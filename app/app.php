d1369ac6f49592e1918a48ce0c2f467ebc836fa6
Brian Kropff & erica cha <student@epicodus.com>
2015-03-04 11:48:14 -0800

    Debug and operates

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
    require_once __DIR__."/../src/Task.php";
 
    session_start();
+
    if (empty($_SESSION['list_of_tasks'])) {
        $_SESSION['list_of_tasks'] = array();
    }
@@ -13,29 +14,35 @@
 
       $output = "";
 
-      $all_tasks = Task::getAll();
+      //$all_tasks = Task::getAll();
 
-      if (!empty($all_tasks)) {
-          $output = $output . "
+      if (!empty(Task::getAll())) {
+          $output .=  "
           <h1>To Do List</h1>
           <p>Here are all your tasks:</p>
           <ul>";
 
-          foreach ($all_tasks as $task){
-              $output = $output . "<p>" . $task->getDescription() . "</p>";}
+          foreach (Task::getAll() as $task){
+              $output .= "<p>" . $task->getDescription() . "</p>";
           }
 
-        $output = $output . "</ul>";
+        $output .= "</ul>";
     }
 
-      $output = $output . "
-        <form action='/task' method='post'>
+        $output .= "
+        <form action='/tasks' method='post'>
             <label for='description'>Task Description</label>
             <input id='description' name='description' type='text'>
 
             <button type='sumbit'>Add task</button>
         </form>
-    ";
+       ";
+
+       $output .= "
+           <form action='/delete_tasks' method='post'>
+               <button type='submit'>Clear</button>
+           </form>
+        ";
 
       return $output;
 
@@ -51,6 +58,16 @@
      ";
 
 
+   });
+
+   $app->post("/delete_tasks", function(){
+
+       Task::deleteAll();
+
+       return "
+            <h1>List Cleared!</h1>
+            <p><a href='/'>Home</a></p>
+            ";
    });
 
    return $app;
--- src/Task.php
+++ src/Task.php
@@ -28,6 +28,11 @@ class Task
         {
             return $_SESSION['list_of_tasks'];
         }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_tasks'] = array();
+        }
 }
 
 ?>

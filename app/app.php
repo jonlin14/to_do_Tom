9efb236f2b1e1b42f25ccb2503f7f0d6d7d6d536
Brian Kropff & erica cha <student@epicodus.com>
2015-03-04 12:08:45 -0800

    correct indentations

--- app/app.php
+++ app/app.php
@@ -1,33 +1,33 @@
 <?php
-   require_once __DIR__."/../vendor/autoload.php";
-   require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
 
-   session_start();
+    session_start();
 
-   if (empty($_SESSION['list_of_tasks'])) {
-       $_SESSION['list_of_tasks'] = array();
-   }
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
 
-   $app = new Silex\Application();
+    $app = new Silex\Application();
 
-   $app->get("/", function() {
+    $app->get("/", function() {
 
-      $output = "";
+        $output = "";
 
       //$all_tasks = Task::getAll();
 
-      if (!empty(Task::getAll())) {
-          $output .=  "
-          <h1>To Do List</h1>
-          <p>Here are all your tasks:</p>
-          <ul>";
+        if (!empty(Task::getAll())) {
+            $output .=  "
+            <h1>To Do List</h1>
+            <p>Here are all your tasks:</p>
+            <ul>";
 
-          foreach (Task::getAll() as $task){
+            foreach (Task::getAll() as $task){
               $output .= "<p>" . $task->getDescription() . "</p>";
-          }
+            }
 
-        $output .= "</ul>";
-    }
+            $output .= "</ul>";
+        }
 
         $output .= "
         <form action='/tasks' method='post'>
@@ -36,41 +36,41 @@
 
             <button type='sumbit'>Add task</button>
         </form>
-       ";
-
-       $output .= "
-           <form action='/delete_tasks' method='post'>
-               <button type='submit'>Clear</button>
-           </form>
         ";
 
-      return $output;
+        $output .= "
+            <form action='/delete_tasks' method='post'>
+            <button type='submit'>Clear</button>
+            </form>
+        ";
 
-   });
+        return $output;
 
-   $app->post("/tasks", function() {
-       $task = new Task($_POST['description']);
-       $task->save();
-       return "
-           <h1>You created a task!</h1>
-           <p>". $task->getDescription()."</p>
-           <p><a href='/'> View your list of things to do.</a></p>
-     ";
+        });
+
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return "
+            <h1>You created a task!</h1>
+            <p>". $task->getDescription()."</p>
+            <p><a href='/'> View your list of things to do.</a></p>
+        ";
 
 
-   });
+        });
 
-   $app->post("/delete_tasks", function(){
+    $app->post("/delete_tasks", function(){
 
-       Task::deleteAll();
+        Task::deleteAll();
 
-       return "
+        return "
             <h1>List Cleared!</h1>
             <p><a href='/'>Home</a></p>
-            ";
-   });
+        ";
+    });
 
-   return $app;
+    return $app;
 
 
  ?>

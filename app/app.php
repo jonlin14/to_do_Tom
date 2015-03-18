7a796181ca3fc056fd5fb516411b0f497d5bf5fd
Tom Mertz & Bojana Skarich <student@epicodus.com>
2015-03-18 10:45:49 -0700

    Clean up useless routes.

--- app/app.php
+++ app/app.php
@@ -15,52 +15,51 @@
     ));
 
 
+    //load homepage and display all the categories from the database
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.twig', array('category' => Category::getAll()));
+        return $app['twig']->render('index.twig', array('category_array' => Category::getAll()));
     });
 
-
-    $app->get("/tasks", function() use ($app) {
-        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    //Add a category to the database by posting and then go to the index.twig
+    //file and list all the available categories
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('index.twig', array('category_array' => Category::getAll()));
     });
 
-
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.twig', array('category' => Category::getAll()));
+    //Once a category has been clicked in the index.twig, go to that
+    //individual category by finding its id in the database and list
+    //the tasks that are associated with that specific category
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category_task.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
-
+    //If we are at a specific category we can add a task to that category only
+    //if we do a post through the form, sends to the /tasks which displays
+    //the specifc category and all associated tasks
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
         $task = new Task($description, $id = null, $category_id);
         $task->save();
         $category = Category::find($category_id);
-        return $app['twig']->render('categories.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+        return $app['twig']->render('category_task.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
-
+    //This directs us to the home page twig file index.twig but it uses the
+    //delete_tasks route to delete all your tasks. It uses this route instead of
+    //loading the ("/") address. Uses clear button.
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.twig', array('category' => Category::getAll()));
-    });
-
-
-    $app->post("/categories", function() use ($app) {
-        $category = new Category($_POST['name']);
-        $category->save();
-        return $app['twig']->render('index.twig', array('category' => Category::getAll()));
-    });
-
-    $app->get("/categories/{id}", function($id) use ($app) {
-        $category = Category::find($id);
-        return $app['twig']->render('categories.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+        return $app['twig']->render('index.twig', array('category_array' => Category::getAll()));
     });
 
 
     $app->post("/delete_categories", function() use ($app) {
         Category::deleteAll();
-        return $app['twig']->render('index.twig');
+        return $app['twig']->render('index.twig', array('category_array' => Category::getAll()));
     });
 
 

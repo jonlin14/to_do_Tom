6a86573af5850c7bf898a21a44f1a44048c902f7
Tom Mertz & Bojana Skarich <student@epicodus.com>
2015-03-18 12:12:06 -0700

    Update delete() to clear the tasks from the category deleted

--- src/Category.php
+++ src/Category.php
@@ -68,7 +68,8 @@
 
         function delete()
         {
-            
+            $GLOBALS['DB']->exec("DELETE FROM categories WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM tasks WHERE category_id = {$this->getId()};");
         }
 
         //Returns a list of all of our tasks by looping through all of the saved tasks, and creates a new object with an array called $categories.

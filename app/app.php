b4f6cd2c6e79594cdb63df8f3fcb423aae06378c
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-23 11:45:20 -0700

    Add delete route in app file

--- app/app.php
+++ app/app.php
@@ -19,5 +19,41 @@
         return $app['twig']->render("index.twig");
     });
 
+    $app->get('/tasks', function() use ($app) {
+
+        return $app['twig']->render("tasks.html.twig", array('tasks' => Task::getAll()));
+    });
+
+    $app->post('/tasks', function() use ($app) {
+        $new_task = new Task($_POST['task_name']);
+        $new_task->save();
+
+        return $app['twig']->render("tasks.html.twig", array('tasks' => Task::getAll()));
+    });
+
+    $app->get('/categories', function() use ($app) {
+        return $app['twig']->render("categories.html.twig", array ('categories' => Category::getAll()));
+    });
+
+    $app->post('/categories', function() use ($app) {
+        $name = $_POST['category_name'];
+        $category = new Category($name);
+        $category->save();
+
+        return $app['twig']->render('categories.html.twig', array ('categories' => Category::getAll()));
+
+    });
+
+    $app->post('/delete_categories', function() use ($app){
+        Category::deleteAll();
+        return $app['twig']->render('categories.html.twig', array ('categories' => Category::getAll()));
+    });
+
+    $app->post('/delete_tasks', function() use ($app) {
+        Task::deleteAll();
+
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
     return $app;
 ?>
